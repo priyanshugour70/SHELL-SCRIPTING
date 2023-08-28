d66e4a952c6ec0945f66df381edf8c36c41bf2af
priyanshugour70 <priyanshugournotes1@gmail.com>
2023-08-28 17:00:48 +0000

    For Loop Script

---
+++ script07.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# For Loop ..!
+
+
+# For Basic Number's 
+for num in 1 2 3 4 5 
+do
+	echo "Number is $num "
+done
+
+
+
+# For String's .. 
+for str in "Priyanshu" "Devanshu" "Suhani" "Harshita"
+do 
+	echo "Name is : $str "
+done
+
+
+
+# For Number's in Range...
+for i in {1..10}
+do
+	echo "Number : $i "
+done
+
+
+
+# For String Again.. 
+for task in Read Write Blog Video Sleep Eat
+do
+	echo "My tasks is : $task"
+done
+
+
+
+
