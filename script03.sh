6575c9b8b236737b04390288c32c6a7e58221871
priyanshugour70 <priyanshugournotes1@gmail.com>
2023-08-28 14:33:04 +0000

    Linux Command Script

---
+++ script03.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Use Linux Command in ShellScripting ..!
+
+echo "My Current Directry is : $(pwd)"
+echo
+
+host=$(hostname)
+
+echo "Hostname of the this Linux Server is : $host ..!"
